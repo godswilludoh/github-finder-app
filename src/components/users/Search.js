e75a01c080e0b48ba31806362040c1d0c2f118e5
Godswill Udoh <godswilludoh@gmail.com>
2022-04-05 07:59:44 +0100

    added searchUsers async await

---
+++ src/components/users/Search.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+	state = {
+		text: '',
+	};
+
+	onInputChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.props.searchUsers(this.state.text);
+		this.setState({ text: '' });
+	};
+
+	render() {
+		return (
+			<div>
+				<form className='form' onSubmit={this.onSubmit}>
+					<input
+						type='text'
+						name='text'
+						placeholder='Search Users...'
+						value={this.state.text}
+						onChange={this.onInputChange}
+					/>
+					<input
+						type='submit'
+						value='Search'
+						className='btn btn-dark btn-block'
+					/>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Search;
