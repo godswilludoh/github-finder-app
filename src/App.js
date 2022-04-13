288c969af948dcff7bb6b9c7f9746db24fbedfe6
Godswill Udoh <godswilludoh@gmail.com>
2022-04-13 14:21:05 +0100

    committed changes to App.js and search.js

--- src/App.js
+++ src/App.js
@@ -22,13 +22,26 @@ class App extends Component {
 		this.setState({ users: res.data.items, loading: false });
 	};
 
+	//clear users from state
+	clearUsers = () => {
+		this.setState({
+			users: [],
+			loading: false,
+		});
+	};
+
 	render() {
+		const { users, loading } = this.state;
 		return (
 			<div>
 				<Navbar title='Github Finder' icon='fa-brands fa-github' />
 				<div className='container'>
-					<Search searchUsers={this.searchUsers} />
-					<Users loading={this.state.loading} users={this.state.users} />
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={users.length > 0 ? true : false}
+					/>
+					<Users loading={loading} users={users} />
 				</div>
 			</div>
 		);
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -18,6 +18,7 @@ class Search extends Component {
 	};
 
 	render() {
+		const { showClear, clearUsers } = this.props;
 		return (
 			<div>
 				<form className='form' onSubmit={this.onSubmit}>
@@ -34,6 +35,11 @@ class Search extends Component {
 						className='btn btn-dark btn-block'
 					/>
 				</form>
+				{showClear && (
+					<button className='btn btn-light btn-block' onClick={clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
